44ab069556580579d6e37043a84e33066184af4e
Antonio Rebordao <antonio@rebordao.net>
2015-03-23 00:02:25 +0000

    Added R solution to problem 16.

---
+++ R/p16.R
@@ -0,0 +1,21 @@
+#### 16 | Power digit sum
+"
+https://projecteuler.net/problem=16
+
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+"
+
+# Writes nr as a string
+nr <- sprintf("%f", 2^1000)
+
+# Splits nr
+nr_splitted <- unlist(strsplit(nr, "", ""))
+
+# Removes "."
+nr_splitted <- subset(
+  nr_splitted, !(1:length(nr_splitted) %in% which(nr_splitted == ".")))
+
+# Computes sum
+print(sum(as.numeric(nr_splitted)))
