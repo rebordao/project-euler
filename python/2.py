927e2afced8dba12d77233924efe59b21721e46b
Charalampos Papaloizou <papaloizouc@gmail.com>
2015-03-25 21:26:13 +0000

    1-4 in python

---
+++ python/2.py
@@ -0,0 +1,26 @@
+import math
+
+
+limit = 4e6  # 4 million
+fi = (1 + math.sqrt(5)) / 2
+
+max_index = (math.log(limit) + math.log(5) / 2) / math.log(fi)
+max_index = int(math.floor(max_index))
+
+
+def fib_number(n, fi):
+    return int((fi**n - (-fi**-n)) / math.sqrt(5))
+
+
+def fib(start, end, exclude=None):
+    for n in range(start, end):
+        number = fib_number(n, fi)
+
+        if filter is None:
+            yield number
+        elif exclude(number) is False:
+            yield number
+
+even_numbers = fib(1, max_index + 1, exclude=lambda x: x % 2 != 0)
+
+print(sum(even_numbers))
---
+++ python/3.py
@@ -0,0 +1,36 @@
+import math
+
+limit = 600851475143
+max_factor = int(math.floor(math.sqrt(limit)))
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    elif n == 2:
+        return True
+    else:
+        for i in range(3, int(math.sqrt(n)) + 1, 2):
+            if n % i == 0:
+                return False
+
+    return True
+
+factors = []
+for n in range(2, max_factor):
+    if limit % n == 0:
+        factors.append(n)
+        factors.append(limit / n)
+
+factors = sorted(factors, key=lambda x: -x)
+
+
+def largest_prime_factor(factors):
+    for factor in factors:
+        if is_prime(factor):
+            return factor
+    else:
+        raise Exception("Not found!")
+
+result = largest_prime_factor(factors)
+print(result)
