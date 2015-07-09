e68e7c640ac46a758559c00294e1d51a7470de56
Antonio Rebordao <antonio@rebordao.net>
2015-07-09 15:06:58 +0100

    Review R 1-5

--- R/p1.R
+++ R/p1.R
@@ -1,5 +1,4 @@
 # 1 | Multiples of 3 and 5
-# 
 # https://projecteuler.net/problem=1
 # 
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, 
@@ -8,4 +7,15 @@
 
 nrs <- 3:999 # we just need to check for the ones between 3 and 999
 idxs <- nrs %% 3 == 0 | nrs %% 5 == 0
-sum(nrs[idxs])
+print(sum(nrs[idxs]))
+
+# Another interesting approach is:
+
+# floor(999 / 3) = 33 thus 
+div_by_3 <- 3 * 1:333
+# floor(999 / 5) = 199 thus 
+div_by_5 <- 5 * 1:199
+# but some numbers are multiples of 3 and 5, and we're counting them twice
+# floor(999 / 15) = 66 thus 
+div_by_15 <- 15 * 1:66
+print(sum(div_by_3) + sum(div_by_5) - sum(div_by_15))
--- R/p5.R
+++ R/p5.R
@@ -1,12 +1,12 @@
 # 5 | Smallest multiple
-# 
 # https://projecteuler.net/problem=5
 # 
 # 2520 is the smallest number that can be divided by each of the numbers from 
 # 1 to 10 without any remainder. What is the smallest positive number that is 
 # evenly divisible by all of the numbers from 1 to 20?
 
-# Tweaked brute-force solution, only takes 32 seconds in my 6 years-old laptop
+#### Tweaked brute-force solution ####
+# takes 23 seconds in my 6 years-old laptop
 
 nr <- 20
 while (sum(nr %% 11:20) > 0L) {
@@ -14,3 +14,32 @@ while (sum(nr %% 11:20) > 0L) {
   nr <- nr + 20L # checks only for multiples of 20
 }
 print(nr)
+
+#### Prime Factorization ####
+
+# Let N be the number that we want to find
+# Then N needs to be divisible by all nrs 2:k
+
+# For k = 2, N = 2,
+# For k = 3, N = 2 * 3
+# For k = 4, N = 2 * 3 * 2
+# For k = 5, N = 2 * 3 * 2 * 5
+# For k = 6, N = 2 * 3 * 2 * 5
+# For k = 7, N = 2 * 3 * 2 * 5 * 7
+# For k = 8, N = 2 * 3 * 2 * 5 * 7 * 2
+# For k = 9, N = 2 * 3 * 2 * 5 * 7 * 2 * 3
+# For k = 10, N = 2 * 3 * 2 * 5 * 7 * 2 * 3
+# For k = 11, N = 2 * 3 * 2 * 5 * 7 * 2 * 3 * 11
+# For k = 12, N = 2 * 3 * 2 * 5 * 7 * 2 * 3 * 11
+# For k = 13, N = 2 * 3 * 2 * 5 * 7 * 2 * 3 * 11 * 13
+# For k = 14, N = 2 * 3 * 2 * 5 * 7 * 2 * 3 * 11 * 13
+# For k = 15, N = 2 * 3 * 2 * 5 * 7 * 2 * 3 * 11 * 13
+# For k = 16, N = 2 * 3 * 2 * 5 * 7 * 2 * 3 * 11 * 13 * 2
+# For k = 17, N = 2 * 3 * 2 * 5 * 7 * 2 * 3 * 11 * 13 * 2 * 17
+# For k = 18, N = 2 * 3 * 2 * 5 * 7 * 2 * 3 * 11 * 13 * 2 * 17
+# For k = 19, N = 2 * 3 * 2 * 5 * 7 * 2 * 3 * 11 * 13 * 2 * 17 * 19
+# For k = 20, N = 2 * 3 * 2 * 5 * 7 * 2 * 3 * 11 * 13 * 2 * 17 * 19
+print(2 * 3 * 2 * 5 * 7 * 2 * 3 * 11 * 13 * 2 * 17 * 19)
+
+# How to do this programmatically?
+# TODO
