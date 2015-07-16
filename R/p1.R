bc8c060842195e440a9de7a18dc2af8cccaf8530
Antonio Rebordao <antonio@rebordao.net>
2015-07-16 02:18:12 +0100

    Add julia 1

--- R/p1.R
+++ R/p1.R
@@ -1,7 +1,7 @@
 # 1 | Multiples of 3 and 5
 # https://projecteuler.net/problem=1
-# 
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+#
+# If we list all the natural numbers below 10 that are multiples of 3 or 5,
 # we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
@@ -11,11 +11,12 @@ print(sum(nrs[idxs]))
 
 # Another interesting approach is:
 
-# floor(999 / 3) = 33 thus 
+# floor(999 / 3) = 33 thus
 div_by_3 <- 3 * 1:333
-# floor(999 / 5) = 199 thus 
+# floor(999 / 5) = 199 thus
 div_by_5 <- 5 * 1:199
 # but some numbers are multiples of 3 and 5, and we're counting them twice
-# floor(999 / 15) = 66 thus 
+# floor(999 / 15) = 66 thus
 div_by_15 <- 15 * 1:66
 print(sum(div_by_3) + sum(div_by_5) - sum(div_by_15))
+
