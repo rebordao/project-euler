0a54b39d0863a307f19638a6ecf029768a9b4b86
charalampos papaloizou <papaloizouc@gmail.com>
2017-05-07 11:42:25 +0100

    fix design of tree

--- c/includes/btree.h
+++ c/includes/btree.h
@@ -5,26 +5,43 @@ typedef int boolean;
 #define true 1
 #define false 0
 
-#define compare_lower(left, right) ({   \
-    int result = 0;                     \
-    if(left > right) {result = 1;}      \
-    if(left < right) {result = -1;}     \
-    result;                             \
-})                                      \
+#define ull unsigned long long
+#define number_of_digits(n) ((ull)round(log10(n)) + 1)
 
+enum number_type {
+    TYPE_S, TYPE_I, TYPE_L,
+    TYPE_F, TYPE_D, TYPE_LL, TYPE_ULL, TYPE_LD
+};
 
-typedef struct node
-{
-    TREE_DATA_TYPE data;
+union value {
+    short _short;
+    int _int;
+    long _long;
+    float _float;
+    double _double;
+    long long _long_long;
+    unsigned long long _unsigned_long_long;
+    long double _long_double;
+};
+
+typedef struct node {
+    enum number_type type;
+    union value value;
+
     struct node* left;
     struct node* right;
 } node;
 
 
-node* create_node(TREE_DATA_TYPE data) ;
+typedef int (*compare_lower_func)(node * left, node * right);
 
-node* insert_node(node *root, TREE_DATA_TYPE data) ;
 
-node* search(node *root,const TREE_DATA_TYPE data) ;
+int ull_compare_lower(node * left, node * right);
+
+node* ull_create_node(ull value) ;
+
+node* insert_node(node *root, node * node, compare_lower_func compare) ;
+
+node* search(node *root, node * node, compare_lower_func compare) ;
 
 void dispose(node* root);
